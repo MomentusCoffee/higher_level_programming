8756d3c96ecd47c6e4e53d085f1ee46aa95a2968
LC <perplexedcoffee@gmail.com>
2021-04-01 04:44:11 -0700

    first commit

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const Square = require('./5-square');
+
+module.exports = class square extends Square {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  let rlist = list.sort(() => 1);
+  return rlist;
+};
