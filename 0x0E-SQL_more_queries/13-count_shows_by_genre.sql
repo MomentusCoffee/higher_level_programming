58c845a82f853ace97efd665f1cd3b93cf8a5ed4
LC <perplexedcoffee@gmail.com>
2021-04-01 03:27:47 -0700

    first commit

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Lists all genres from pytn_0d_tvshows and displays the number of shows linked to each
+SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
+FROM tv_show_genres
+     JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
+GROUP BY tv_genres.name
+ORDER BY number_shows DESC;
